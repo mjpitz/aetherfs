941b1b347d454eaa0a74bd69dab3bbc63999704d
Mya Pitzeruse <j.mya.pitz@gmail.com>
2021-09-06 17:52:31 -0500

    add legal header

--- internal/flagset/extract_test.go
+++ internal/flagset/extract_test.go
@@ -1,3 +1,9 @@
+// Copyright (C) The AetherFS Authors - All Rights Reserved
+//
+// Proprietary and confidential.
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Written by Mya Pitzeruse, September 2021
+
 package flagset_test
 
 import (
