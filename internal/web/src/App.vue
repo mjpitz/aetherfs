71a63da83071066634c360832249b09e060c6ce8
Mya Pitzeruse <j.mya.pitz@gmail.com>
2021-10-13 11:12:29 -0500

    added router support and minimal css framework

--- internal/web/src/App.vue
+++ internal/web/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <router-link class="button" to="/">Home</router-link>
+    <router-link class="button" to="/datasets">Datasets</router-link>
+  </div>
+
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+@import "~mini.css/dist/mini-default.min.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- internal/web/vue.config.js
+++ internal/web/vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
     publicPath: '/ui/',
+    css: {
+        requireModuleExtension: false,
+    },
 }
